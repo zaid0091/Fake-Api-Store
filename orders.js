0e190277b723d8eb5dd155d968dad446c28bf8a2
Zaid Liaqat <zaidliaqat999@gmail.com>
2026-02-05 13:02:45 +0500

    :

--- orders.js
+++ orders.js
@@ -7,7 +7,7 @@ if (orders.length === 0) {
 }else {
     ordersDiv.innerHTML = orders.map((order, index) => `
         <div style="margin-left: 20px; margin-right:20px;" class="order">
-            <h3 style="color: black;">Order #${index + 1}</h3>
+            <h3 style="color: black;">Order: ${index + 1}</h3>
             ${order.map(item => `
                 <div style="border: 1px solid #ccc; padding:10px; margin:10px 0; border-radius:6px; display:flex; gap:20px; align-items:center;">
                     <img src="${item.image}" alt="${item.title}" style="width:80px; height:80px; object-fit:contain">
