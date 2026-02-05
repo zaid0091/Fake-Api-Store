f996cf15c7ebf599bb45fcd3e6f6110e3380c1f5
Zaid Liaqat <zaidliaqat999@gmail.com>
2026-02-05 13:05:48 +0500

    ;;

--- orders.js
+++ orders.js
@@ -11,6 +11,7 @@ if (orders.length === 0) {
             ${order.map(item => `
                 <div style="border: 1px solid #ccc; padding:10px; margin:10px 0; border-radius:6px; display:flex; gap:20px; align-items:center;">
                     <img src="${item.image}" alt="${item.title}" style="width:80px; height:80px; object-fit:contain">
+                    <p style="font-weight:semi-bold; color:#333;">${item.description}</p>
                     <p style="font-weight: bold; color: #333;">${item.title}</p>
                     <p style="color: #666;">Quantity: ${item.qty}</p>
                     <p style="color: #000;">Price: $${item.price}</p>
